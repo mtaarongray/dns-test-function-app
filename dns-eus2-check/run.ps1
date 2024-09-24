07984c40983b64ad8d05563b4bb36b54eb69c58d
actionsrunner1 <svc_actionsrunner1@milwaukeetool.com>
2024-09-24 10:21:00 -0500

    revised structure

--- dns-eus2-check/run.ps1
+++ dns-eus2-check/run.ps1
@@ -1,5 +1,13 @@
-param(
-    [string]$foo = "dns-eus2-check"
-)
+# Input bindings are passed in via param block.
+param($Timer)
 
-Write-Output "Hello from $foo"
+# Get the current universal time in the default string format
+$currentUTCtime = (Get-Date).ToUniversalTime()
+
+# The 'IsPastDue' porperty is 'true' when the current function invocation is later than scheduled.
+if ($Timer.IsPastDue) {
+    Write-Host "PowerShell timer is running late!"
+}
+
+# Write an information log with the current time.
+Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
--- dns-test-function-app/run.ps1
+++
@@ -1,5 +0,0 @@
-param(
-    [string]$foo = "dns-test-function-app"
-)
-
-Write-Output "Hello from $foo"
