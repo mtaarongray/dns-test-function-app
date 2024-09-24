1d1d7ef01e3edfc062f77a76f8de9017069f576b
actionsrunner1 <svc_actionsrunner1@milwaukeetool.com>
2024-09-24 10:37:06 -0500

    added lookup logic

--- dns-eus2-check/run.ps1
+++ dns-eus2-check/run.ps1
@@ -11,3 +11,37 @@ if ($Timer.IsPastDue) {
 
 # Write an information log with the current time.
 Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
+
+$AppName = "sapp-p-cus-sql-2-sql.database.windows.net."
+$DnsServers = @(
+    "10.202.0.4",
+    "10.202.0.5",
+    "10.204.0.4",
+    "10.204.0.5"
+)
+
+$ResultOutput = @()
+foreach ($Server in $DnsServers) {
+    $Props = @{
+        Server = $Server
+        AppName = $AppName
+        LookupResult = ""
+        ServiceState = ""
+    }
+    $DnsResult = Resolve-DnsName -Name $AppName -Server $Server
+    if ($DnsResult.QueryResults.Count -gt 0) {
+        $Props.LookupResult = $DnsResult.QueryResults.IPAddress
+        $Props.ServiceState = "UP"
+    } else {
+        $Props.LookupResult = "FAILED"
+        $ConnTest = Test-NetConnection -ComputerName $Server -Port 53
+        if ($ConnTest.TcpTestSucceeded) {
+            $Props.ServiceState = "UP"
+        } else {
+            $Props.ServiceState = "UNREACHABLE"
+        }
+    }
+    $ResultOutput += New-Object PSObject -Property $Props
+}
+
+$ResultOutput | Format-Table -AutoSize
