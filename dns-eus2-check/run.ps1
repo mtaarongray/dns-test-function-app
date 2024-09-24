1b58b9faaa92c08a63ca54c4fd36daafbd34cca4
actionsrunner1 <svc_actionsrunner1@milwaukeetool.com>
2024-09-24 13:17:05 -0500

    test

--- dns-eus2-check/run.ps1
+++ dns-eus2-check/run.ps1
@@ -10,40 +10,38 @@ if ($Timer.IsPastDue) {
 }
 
 $AppName = "sapp-p-cus-sql-2-sql.database.windows.net."
-Resolve-DnsName -Name $AppName
-# $DnsServers = @(
-#     "10.204.0.4",
-#     "10.204.0.5"
-# )
+$DnsServers = @(
+    #"10.204.0.9",
+    "10.204.0.5"
+)
 
-# $Output = @()
-# foreach ($Server in $DnsServers) {
-#     $Props = @{
-#         DateTime = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
-#         Server = $Server
-#         AppName = $AppName
-#         LookupResult = ""
-#         ServiceState = ""
-#         TimeTaken = ""
-#     }
-#     $DnsResult = (nslookup $AppName $server 2>$null | sls address | select -last 1).ToString().Split(":")[1].Trim()
-#     if ($DnsResult.Count -gt 0) {
-#         $Props.ServiceState = "UP"
-#         $Props.LookupResult = $DnsResult
-#     }
-#     else {
-#         $Props.LookupResult = "FAILED"
-#         $ConnTest = Test-NetConnection -ComputerName $Server -Port 53
-#         if ($ConnTest.TcpTestSucceeded) {
-#             $Props.ServiceState = "UP"
-#         } else {
-#             $Props.ServiceState = "UNREACHABLE"
-#         }
-#     }
-#     $Output += New-Object PSObject -Property $Props
-# }
+$Output = @()
+foreach ($Server in $DnsServers) {
+    $Props = @{
+        DateTime = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+        Server = $Server
+        AppName = $AppName
+        LookupResult = ""
+        ServiceState = ""
+    }
+    $DnsResult = (nslookup $AppName $server 2>$null | sls address | ? {$_.linenumber -eq 5}).ToString().Split(":")[1].Trim()
+    if ($DnsResult.Count -gt 0) {
+        $Props.ServiceState = "UP"
+        $Props.LookupResult = $DnsResult
+    }
+    else {
+        $Props.LookupResult = "FAILED"
+        $ConnTest = Test-NetConnection -ComputerName $Server -Port 53
+        if ($ConnTest.TcpTestSucceeded) {
+            $Props.ServiceState = "UP"
+        } else {
+            $Props.ServiceState = "UNREACHABLE"
+        }
+    }
+    $Output += New-Object PSObject -Property $Props
+}
 
-# $Output | Format-Table -AutoSize
+$Output | Format-Table -AutoSize
 
 # foreach ($Entry in $Output) {
 #     if ($Entry.LookupResult -eq "FAILED") {
