334785a7e61b81d51e76c4fe22d507ac4dd53df2
actionsrunner1 <svc_actionsrunner1@milwaukeetool.com>
2024-09-24 12:40:48 -0500

    module test

--- dns-eus2-check/run.ps1
+++ dns-eus2-check/run.ps1
@@ -10,28 +10,49 @@ if ($Timer.IsPastDue) {
 }
 
 $AppName = "sapp-p-cus-sql-2-sql.database.windows.net."
-$DnsServers = @(
-    "10.204.0.4",
-    "10.204.0.5"
-)
+Resolve-DnsName -Name $AppName
+# $DnsServers = @(
+#     "10.204.0.4",
+#     "10.204.0.5"
+# )
 
-foreach ($Server in $DnsServers) {
-    $Props = @{
-        DateTime = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
-        Server = $Server
-        AppName = $AppName
-        LookupResult = ""
-        # ServiceState = ""
-        # TimeTaken = ""
-    }
-    $DnsResult = (nslookup $AppName $server 2>$null | sls address | select -last 1).ToString().Split(":")[1].Trim()
-    $Props.LookupResult = $DnsResult
-    $Obj = New-Object PSObject -Property $Props
-    Write-Output $Obj
-}
+# $Output = @()
+# foreach ($Server in $DnsServers) {
+#     $Props = @{
+#         DateTime = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+#         Server = $Server
+#         AppName = $AppName
+#         LookupResult = ""
+#         ServiceState = ""
+#         TimeTaken = ""
+#     }
+#     $DnsResult = (nslookup $AppName $server 2>$null | sls address | select -last 1).ToString().Split(":")[1].Trim()
+#     if ($DnsResult.Count -gt 0) {
+#         $Props.ServiceState = "UP"
+#         $Props.LookupResult = $DnsResult
+#     }
+#     else {
+#         $Props.LookupResult = "FAILED"
+#         $ConnTest = Test-NetConnection -ComputerName $Server -Port 53
+#         if ($ConnTest.TcpTestSucceeded) {
+#             $Props.ServiceState = "UP"
+#         } else {
+#             $Props.ServiceState = "UNREACHABLE"
+#         }
+#     }
+#     $Output += New-Object PSObject -Property $Props
+# }
 
-# Write an information log with the current time.
-Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
+# $Output | Format-Table -AutoSize
+
+# foreach ($Entry in $Output) {
+#     if ($Entry.LookupResult -eq "FAILED") {
+#         Write-Error -Message "DNS lookup failed for $AppName on server $Server, service state: $Entry.ServiceState"
+#     }
+# }
+
+# # Write an information log with the current time.
+# Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
 
 # $sb = {
 #     param($AppName, $Server)
